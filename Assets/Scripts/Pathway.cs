c233901bea13aeee6b512db8c931f782fd078d08
Jonathan Nguyen <jonthnngyn@gmail.com>
2015-03-20 19:08:42 -0700

    Implement moving spike block and import Adams map

---
+++ Assets/Scripts/Pathway.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Pathway : MonoBehaviour {
+
+	public Transform[] Points;
+
+	public IEnumerator<Transform> GetPathsEnumerator(){
+		//Check if the list of current Points is either null or has 0 elements
+		if(Points == null || Points.Length < 1) yield break;
+
+		int direction = 1;
+		int index = 0;
+
+		/*This gets ran whenever MoveNext() is called. This enumerates the list in a way that if the platform is at the end of its path, its direction gets reversed and
+		the index of the list starts to loop backwards*/
+		while(true){
+			yield return Points[index];
+
+			//If the length of the Points is 1, then just make the platform stay there and do nothing
+			if(Points.Length == 1) continue;
+
+			if(index <= 0) direction = 1;
+			else if(index >= Points.Length - 1) direction = -1;
+
+			index += direction;
+		}
+
+	}
+
+	//Similar to OnGUI() in the sense the when Unity sees this method it automatically gets ran. This allows us to see the line of the path while still in the scene
+	public void OnDrawGizmos(){
+		if(Points == null || Points.Length < 2) return;
+
+        var points = Points.Where(t => t != null).ToList(); //Convert the array to an arraylist. Prevents crash from when a point get removed in the scene because of the null point
+        if (points.Count < 2) return;
+
+		for(int i = 1; i < points.Count; i++){
+			Gizmos.DrawLine(points[i-1].position, points[i].position);
+		}
+	}
+}
