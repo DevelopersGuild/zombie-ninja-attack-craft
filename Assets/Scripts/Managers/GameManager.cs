12ada4de0282bb229f1d6741280a01affddbfa19
Joseph <madhatters@gmail.com>
2015-05-15 13:48:32 -0700

    Commit

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,20 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent (typeof (NotificationManager))]
+[RequireComponent(typeof(NotificationManager))]
 public class GameManager : MonoBehaviour
 {
-
-
-     public static int points;
-     
-     public static GameManager Instance 
-     { 
-          get 
+     public static GameManager Instance
+     {
+          get
           {
                if (instance == null) instance = new GameObject("GameManager").AddComponent<GameManager>();
                return instance;
-          } 
+          }
      }
 
      public static NotificationManager Notifications
@@ -28,9 +24,22 @@ public class GameManager : MonoBehaviour
                return notifications;
           }
      }
+
+     public static LoadAndSaveManager StateManager
+     {
+          get
+          {
+               if (stateManager == null)
+               {
+                    stateManager = instance.GetComponent<LoadAndSaveManager>();
+
+               }
+               return stateManager;
+          }
+     }
      void Awake()
      {
-          if((instance) && (instance.GetInstanceID() != GetInstanceID()))
+          if ((instance) && (instance.GetInstanceID() != GetInstanceID()))
           {
                DestroyImmediate(gameObject);
           }
@@ -43,12 +52,18 @@ public class GameManager : MonoBehaviour
 
      private static GameManager instance = null;
      private static NotificationManager notifications = null;
+     private static LoadAndSaveManager stateManager = null;
+     private static int currentLevel;
 
+     public static int points;
 
      // Use this for initialization
      void Start()
      {
+          currentLevel = Application.loadedLevel;
           points = 0;
+          LoadGameData();
+
      }
 
      public static void AddPoints(int added)
@@ -75,6 +90,38 @@ public class GameManager : MonoBehaviour
      {
           return points;
      }
+
+     public void LoadGameData()
+     {
+          StateManager.Load(Application.persistentDataPath + "/SaveGame.xml");
+     }
+
+     public static void LevelComplete()
+     {
+          bool isActive = StateManager.isActiveAndEnabled;
+          if(stateManager.GameState.GameLevels.Count > currentLevel)
+          {
+               if(stateManager.GameState.GameLevels[currentLevel - 1].Score < points)
+               {
+                    stateManager.GameState.GameLevels[currentLevel - 1].Score = points;
+               }
+
+          }
+          else
+          {
+               LoadAndSaveManager.GameStateData.GameLevelData newLevel = new LoadAndSaveManager.GameStateData.GameLevelData();
+               newLevel.LevelUnlocked = true;
+               newLevel.Score = points;
+               stateManager.GameState.GameLevels.Add(newLevel);
+          }
+     }
+
+     public void SaveGame()
+     {
+          LevelComplete();
+          StateManager.Save(Application.persistentDataPath + "/SaveGame.xml");
+     }
+
 
      public void SwitchLevel(int level)
      {
