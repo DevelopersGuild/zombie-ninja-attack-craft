3790ef1aa3396945cb6dcf1c6b55950df8fdd8aa
Jonathan Nguyen <jonthnngyn@gmail.com>
2015-05-15 19:15:19 -0700

    Add a key and door mechanic

---
+++ Assets/Scripts/Level Objects/Door.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+    Sprite opened;
+    SpriteRenderer sr;
+    private BoxCollider2D collider;
+	// Use this for initialization
+	void Start () {
+        collider = GetComponent<BoxCollider2D>();
+        sr = GetComponent<SpriteRenderer>();
+	}
+
+    // Open the door by disabling the collider and changin the sprite 
+    public void OpenDoor() {
+        collider.enabled = false;
+        if (opened != null) {
+            sr.sprite = opened;
+        }else{
+            sr.enabled = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Level Objects/DoorKey.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorKey : MonoBehaviour {
+
+    public Door door;
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Player") {
+            door.OpenDoor();
+            //PLAY SOUND
+            Destroy(gameObject);
+        }
+    }
+}
