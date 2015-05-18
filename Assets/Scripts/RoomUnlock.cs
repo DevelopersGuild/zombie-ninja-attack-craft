ba74f31da60fc184d853dc4108986323227759c6
Jonathan Nguyen <jonthnngyn@gmail.com>
2015-05-17 23:32:42 -0700

    Rooms can drop rewards

--- Assets/Scripts/RoomUnlock.cs
+++ Assets/Scripts/RoomUnlock.cs
@@ -6,36 +6,38 @@ public class RoomUnlock : MonoBehaviour {
 
     public List<Door> doors = new List<Door>();
     public List<GameObject> enemies = new List<GameObject>();
-    private int enemyCount;
-    private bool isCleared;
+    public List<GameObject> rewards = new List<GameObject>();
 
+    private bool isCleared;
     private bool isActive;
 
-	// Use this for initialization
-	void Start () {
-        enemyCount = 0;
+    // Make sure the doors are open and the enemies arent spawned
+    void Start() {
         isCleared = false;
         isActive = false;
+
         foreach (Door door in doors) {
             door.OpenDoor();
         }
         foreach (GameObject enemy in enemies) {
             enemy.SetActive(false);
-            enemyCount++;
         }
-	}
+        foreach (GameObject reward in rewards) {
+            reward.SetActive(false);
+        }
+    }
 
+    // Check for the player to activate the room
     void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Player") {
             ActivateRoom();
         }
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update() {
+        // Check for whether or not all the enemies in the room have died
         if (isActive) {
-            Debug.Log(enemies.Count);
-            Debug.Log(isCleared);
             isCleared = true;
             foreach (GameObject enemy in enemies) {
                 if (enemy != null) {
@@ -45,11 +47,10 @@ public class RoomUnlock : MonoBehaviour {
             if (isCleared) {
                 closeRoom();
             }
-
-
         }
-	}
+    }
 
+    // Close the doors and spawn all the enemies
     void ActivateRoom() {
         foreach (Door door in doors) {
             door.CloseDoor();
@@ -60,9 +61,13 @@ public class RoomUnlock : MonoBehaviour {
         isActive = true;
     }
 
+    // Open the doors and spawn the reward items
     void closeRoom() {
         foreach (Door door in doors) {
             door.OpenDoor();
+        }
+        foreach (GameObject reward in rewards) {
+            reward.SetActive(true);
         }
         Destroy(gameObject);
     }
