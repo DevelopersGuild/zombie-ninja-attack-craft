b4b725b154b37bb82efd78b04f8d4e2c2490ef20
Joseph <madhatters@gmail.com>
2015-06-05 14:47:44 -0700

    Player progression fixed

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -241,7 +241,6 @@ public class GameManager : MonoBehaviour
           stateManager.GameState.Player.DashSpeed = 0;
           stateManager.GameState.Player.IsLandMineUnlocked = true;
           stateManager.GameState.Player.StartingHealth = 0;
-          SaveGame();
           GameManager.Notifications.PostNotification(this, "LevelLoaded");
      }
 }
