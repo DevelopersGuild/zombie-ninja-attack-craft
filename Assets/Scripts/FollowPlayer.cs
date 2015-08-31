7bcdb6853e2e67a15d3f432efcf086e61f88c545
Jonathan Nguyen <jonthnngyn@gmail.com>
2015-08-31 13:16:59 -0700

    Enemy balance changes with porcupines and snakes, level 2 balancing, add charge particles

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer: MonoBehaviour {
+
+     private Transform transform;
+     public Transform target;
+
+	// Use this for initialization
+	void Start () {
+          transform = GetComponent<Transform>();
+          target = FindObjectOfType<Player>().GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+          transform.position = target.position;
+	}
+}
