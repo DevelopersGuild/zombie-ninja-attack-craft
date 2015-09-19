cb4b5da29b78a7d71cbf6778e6c5559396e297bb
Johnny Nguyen <jonthnngyn@gmail.com>
2015-09-19 16:06:40 -0700

    Fix layering and lighting, catch up on lighting

--- Assets/Scripts/EnableInRange.cs
+++ Assets/Scripts/EnableInRange.cs
@@ -3,13 +3,29 @@ using System.Collections;
 
 public class EnableInRange : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    private Player player;
+    public float maxDistance;
+    private Light light;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+        light = GetComponent<Light>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
+        Vector3 direction = transform.position - player.transform.position;
+        float distance = direction.magnitude;
+        Debug.Log(distance);
+	    if(distance < maxDistance)
+        {
+            light.enabled = true;
+        }
+        else
+        {
+            light.enabled = false;
+        }
 	}
 }
