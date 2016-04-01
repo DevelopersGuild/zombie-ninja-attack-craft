446cd665cdd5d0618002c03617c67f5887b611e3
Johnny <jonthnngyn@gmail.com>
2016-03-31 23:10:21 -0700

    Make minimap toggleable, Upgrade to latest version of unity, add instructions for map

---
+++ Assets/Scripts/CanvasControllers/MinimapCamera.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinimapCamera : MonoBehaviour
+{
+    private bool isActive;
+    private Camera camera;
+
+    void Start()
+    {
+        isActive = false;
+        camera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+	void Update ()
+    {
+	    if (Input.GetButtonDown("Map") && isActive)
+	    {
+            Debug.Log("enabling!");
+	        camera.enabled = false;
+	        isActive = false;
+	    }
+	    else if(Input.GetButtonDown("Map") && !isActive)
+	    {
+	        camera.enabled = true;
+	        isActive = true;
+	    }
+	}
+}
