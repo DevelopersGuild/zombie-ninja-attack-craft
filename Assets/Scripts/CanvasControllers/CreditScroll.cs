fe922a1ef1c6f28fe14ed0d882f86dd7f67a067f
Jonathan Nguyen <jonthnngyn@gmail.com>
2016-09-02 16:08:28 -0700

    Credits scene

---
+++ Assets/Scripts/CanvasControllers/CreditScroll.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditScroll : MonoBehaviour {
+
+    public float speed;
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.up * Time.deltaTime * speed);
+	}
+}
